e55a7770e61321d2db50b31b4fd3150c9463de05
hojmax <axelhojmark@gmail.com>
2023-01-13 13:23:06 +0100

    Update gpt.py

--- gpt.py
+++ gpt.py
@@ -4,28 +4,7 @@ generator = pipeline('text2text-generation', model='t5-large', device="cuda:0")
 set_seed(0)
 
 prompt = (
-    """Given the grammar:
-walk = WALK
-look = LOOK
-run = RUN
-jump = JUMP
-turn left = LTURN
-turn right = RTURN
-u left = LTURN u
-u right = RTURN u
-turn opposite left = LTURN LTURN
-turn opposite right = RTURN RTURN
-u opposite left = turn opposite left u
-u opposite right = turn opposite right u
-turn around left = LTURN LTURN LTURN LTURN
-turn around right = RTURN RTURN RTURN RTURN
-u around left = LTURN u LTURN u LTURN u LTURN u
-u around right = RTURN u RTURN u RTURN u RTURN u
-x twice = x x
-x thrice = x x x
-x1 and x2 = x1 x2
-x1 after x2 = x2 x1
-And these examples:
+    """Given these examples:
 IN: run right twice and walk opposite right twice OUT: RTURN RUN RTURN RUN RTURN RTURN WALK RTURN RTURN WALK
 IN: look right twice after turn right OUT: RTURN RTURN LOOK RTURN LOOK
 IN: look twice and run opposite left OUT: LOOK LOOK LTURN LTURN RUN
@@ -35,11 +14,13 @@ IN: look around left after turn opposite left twice OUT: LTURN LTURN LTURN LTURN
 IN: turn left after run twice OUT: RUN RUN LTURN
 IN: jump OUT: JUMP
 Translate the following:
-IN: turn left after jump twice OUT:"""
+turn left after jump twice"""
 )
 
 output = generator(prompt, max_new_tokens=50,
                    max_length=None)
+
+print(output)
 
 for i, sequence in enumerate(output):
     print(i, sequence['generated_text'][len(prompt):].split("\n")[0])
